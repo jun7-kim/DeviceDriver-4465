5d51eeec942e3fc072506d86ab361b1078ee6a2c
Jun Kim <jun7.kim@gmail.com>
2023-06-05 13:59:48 +0900

    Initial Version

---
+++ Sample-Test1/test.cpp
@@ -0,0 +1,9 @@
+#pragma once
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+#include "../DeviceDriver/DeviceDriver.cpp"
+
+TEST(TestCaseName, TestName) {
+  EXPECT_EQ(1, 1);
+  EXPECT_TRUE(true);
+}
