c0b2b4d03adec9cc8f4dd5aa3f12798f09953841
Jun Kim <jun7.kim@gmail.com>
2023-06-05 15:18:37 +0900

    Implement read operation

--- DeviceDriver/DeviceDriver.cpp
+++ DeviceDriver/DeviceDriver.cpp
@@ -1,16 +1,45 @@
-#include "DeviceDriver.h"
+#include <exception>
 
-DeviceDriver::DeviceDriver(FlashMemoryDevice* hardware) : m_hardware(hardware)
+#include "DeviceDriver.h"
+#include "windows.h"
+
+using namespace std;
+
+class ReadFailException : public exception
+{
+public:
+	const char* what()
+	{
+		return "ReadFailException is occurred";
+	}
+};
+
+DeviceDriver::DeviceDriver(FlashMemoryDeviceAPI* hardware) : m_hardware(hardware)
 {}
 
 int DeviceDriver::read(long address)
 {
-    // TODO: implement this method properly
-    return (int)(m_hardware->read(address));
+	int value = (int)(m_hardware->read(address));
+	int new_value;
+	for (int i = 0; i < (repeat_read_cnt - 1); i++)
+	{
+		waitDelay();
+		new_value = (int)(m_hardware->read(address));
+		if (value != new_value) {
+			throw ReadFailException();
+		}
+	}
+
+	return value;
 }
 
 void DeviceDriver::write(long address, int data)
 {
-    // TODO: implement this method
-    m_hardware->write(address, (unsigned char)data);
+	// TODO: implement this method
+	m_hardware->write(address, (unsigned char)data);
+}
+
+void DeviceDriver::waitDelay()
+{
+	Sleep(200);
 }
